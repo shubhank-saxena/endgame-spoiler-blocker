c1a05a497427f19b91514a75cf708c9f43b373b4
shubhank-saxena <saxena.shubhank.19@gmail.com>
2019-04-26 10:55:18 +0530

    First Commit

--- js/blocking.js
+++ js/blocking.js
@@ -1,4 +1,9 @@
-var dictionary = [ 'tyrion', 'jamie', 'cersei', 'daenerys', 'jon', 'snow', 'lannister', 'baelish', 'petyr', 'arya', 'stark', 'sansa', 'greyjoy', 'game of thrones', 'baratheon', 'frey', 'martell', 'tully', 'tyrel', 'westeros', 'night\'s watch', 'bolton', 'clegane', 'targaryen', 'varys' ];
+var dictionary = ['Endgame','Tony Stark', 'Iron Man', 'Pepper Potts', 'Rescue', 'James Rhodes','War Machine','Nick Fury','Nicholas Joseph Fury','Howard Stark','Bruce Banner','Hulk','Natasha Romanoff','Black Widow','Anton Vanko','Whiplash',
+'Boris Turgenov','Crimson Dynamo','Justin Hammer','Igor Vanko','Peter Parker','Spider-Man','Thor','Jane Foster','Odin','Loki','Heimdall','Clint Barton','Hawkeye','Ronin',
+'Steve Rogers','Captain America','Johann Shmidt','Red Skull','Peggy Carter','Bucky Barnes','Winter Soldier','Hunter','White Wolf','Arnim Zola','Thanos','Wanda Maximoff','Scarlet Witch',
+'Peter Quill','Star-Lord','Gamora','Arthur Douglas','Drax the Destroyer','Rocket Raccoon','Groot','Rhomann Dey','Nova Prime','Garthan Saal','Nova Omega','Nebula','Ronan the Accuser','Korath the Pursuer',
+'Kraglin','Cosmo','Karman-Kan','Vision','Hank','Pym','Ant-Man','Scott Lang','Ant-Man','Darren Cross','Yellow jacket','Hope Pym','Red Queen','Janet van Dyne','Wasp',"T'Challa",'Black Panther','Helmut Zemo','Baron Zemo','Everett Ross',
+'Shuri','Nakia','Stephen Strange','Doctor Strange','Dormammu','Mantis','Valkyrie','Surtur','Carol Danvers','Captain Marvel','Yon-Rogg','Magnitron','Minn-Erva','Doctor Minerva','Maria Rambeau','Ikaris','Sersi','Gamora'];
 
 function replace() {
 	tags = document.getElementsByTagName('*');
@@ -11,7 +16,7 @@ function replace() {
 
 			if ( node.nodeType === 3 ) {
 				var text = node.nodeValue;
-				text = text.toLowerCase();
+				text = text.toString();
 				dictionary.forEach( function( dictionaryItem ) {
 					var newText = text.includes( dictionaryItem );
 					if ( newText ) {
